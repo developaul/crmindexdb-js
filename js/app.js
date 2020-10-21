e8eb884c6331f5e756a0868644e11152aadf1e75
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-21 16:26:05 -0500

    eliminarRegistro added

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@ import { listadoClientes } from './references.js';
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                             <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                         </td>
                     </tr>`;
                     
@@ -73,12 +73,38 @@ import { listadoClientes } from './references.js';
         }
     }
 
+    // Elimina un registro 
+    const eliminarRegistro = event => {
+        if( event.target.classList.contains( 'eliminar' ) ){
+            const idEliminar = Number( event.target.dataset.cliente );
+            const confirmar = confirm( 'Deseas eliminar este cliente?' );
+
+            if( confirmar ){
+                const transaction = DB.transaction( ['crm'], 'readwrite' );
+                const objectStore = transaction.objectStore( 'crm' );
+
+                objectStore.delete( idEliminar );
+
+                transaction.oncomplete = () => {
+                    console.info( 'Eliminado..' );
+                    event.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = () => {
+                    console.error( 'Hubo un error' );
+                }
+            }
+        }
+    } 
+
     document.addEventListener( 'DOMContentLoaded', () => {
         crearDB();
 
         if( window.indexedDB.open( 'crm', 1 ) ) {
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener( 'click', eliminarRegistro );
     });
 
 })();
