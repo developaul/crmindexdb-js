42f31a866735e0a9298a64e8c1ec5da03a84eb97
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-21 14:55:03 -0500

    references added

---
+++ js/references.js
@@ -0,0 +1,15 @@
+
+// References
+const form = document.querySelector( '#formulario' ),
+      nombreInput = document.querySelector( '#nombre' ), 
+      emailInput = document.querySelector( '#email' ),
+      telefonoInput = document.querySelector( '#telefono' ), 
+      empresaInput = document.querySelector( '#empresa' ); 
+
+export {
+    form,
+    nombreInput,
+    emailInput,
+    telefonoInput,
+    empresaInput
+}
