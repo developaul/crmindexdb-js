d2c1f247a41a5255ce18bbb8891383ac82043a01
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-21 15:25:25 -0500

    obtenerClientes added, get the clients from the database and print them to the html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { listadoClientes } from './references.js';
+
 (() =>{
     'use strict';
     let DB;
@@ -27,8 +29,56 @@
         }
     }
 
+    // Obtiene los clientes de la base de datos
+    const obtenerClientes = () => {
+        const abrirConexion = window.indexedDB.open( 'crm', 1 );
+
+        abrirConexion.onerror = () => {
+            console.error( 'Hubo un error' );
+        }
+
+        abrirConexion.onsuccess = () => {
+            DB = abrirConexion.result;
+
+            const objectStore = DB.transaction( ['crm'] ).objectStore( 'crm' );
+
+            // Abriendo cursor
+            objectStore.openCursor().onsuccess = event => {
+                const cursor = event.target.result;
+
+                if( cursor ) {
+                    const { nombre, email, telefono, empresa, id } = cursor.value;
+
+                    listadoClientes.innerHTML += ` 
+                    <tr>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                            <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                            <p class="text-gray-700">${telefono}</p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                            <p class="text-gray-600">${empresa}</p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                            <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                        </td>
+                    </tr>`;
+                    
+                    cursor.continue();
+                }
+            }
+        }
+    }
+
     document.addEventListener( 'DOMContentLoaded', () => {
         crearDB();
+
+        if( window.indexedDB.open( 'crm', 1 ) ) {
+            obtenerClientes();
+        }
     });
 
 })();
--- js/references.js
+++ js/references.js
@@ -1,15 +1,17 @@
 
 // References
-const form = document.querySelector( '#formulario' ),
-      nombreInput = document.querySelector( '#nombre' ), 
-      emailInput = document.querySelector( '#email' ),
-      telefonoInput = document.querySelector( '#telefono' ), 
-      empresaInput = document.querySelector( '#empresa' ); 
+const form              = document.querySelector( '#formulario' ),
+      nombreInput       = document.querySelector( '#nombre' ), 
+      emailInput        = document.querySelector( '#email' ),
+      telefonoInput     = document.querySelector( '#telefono' ), 
+      empresaInput      = document.querySelector( '#empresa' ),
+      listadoClientes   = document.querySelector( '#listado-clientes' );
 
 export {
     form,
     nombreInput,
     emailInput,
     telefonoInput,
-    empresaInput
+    empresaInput,
+    listadoClientes
 }
